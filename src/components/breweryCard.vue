6e97c98352b52ecb05bd62b175c9cfe270bacd32
Zazzaro <sassaro01cristian@gmail.com>
2023-11-30 16:31:30 +0100

    finished exercise

---
+++ src/components/breweryCard.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    name: "breweryCard",
+    props: {
+        info: Object
+    }
+}
+</script>
+
+<template>
+    <div class="card-wrapper">
+        <div class="card">
+            <h2>{{ info.name }}</h2>
+            <p>{{ info.brewery_type }}</p>
+            <p>{{ info.address_1 }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card-wrapper {
+    padding: 1rem;
+    width: 50%;
+}
+
+.card {
+    padding: 1rem;
+    background: orangered;
+    color: white;
+}
+</style>
+
